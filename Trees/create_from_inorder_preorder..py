8464e27e1a304cb8092e1ab1b8aee1026e8e565b
chaturchatur <ritvikchaturvedi23@gmail.com>
2025-06-05 01:05:48 -0400

    added O(n) hashmap solution

--- Trees/create_from_inorder_preorder..py
+++ Trees/create_from_inorder_preorder..py
@@ -5,6 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # O(n^2) solution
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         # preorder = node, left, right
         # inorder = left, node, right
@@ -19,8 +20,9 @@ class Solution:
         # find the root in inorder to determine the left and right subtrees 
         # elements to the left of 'mid' in inorder are in the left subtree
         # elements to the right of 'mid' in inorder are in the right subtree
-        mid = inorder.index(root.val) 
+        mid = inorder.index(root.val)  # O(n) to search
         
+        # O(n) for recursion
         # the number of elements on the left/right (from inorder) tells us how to split preorder 
         # for left child => split preorder from element after root to number of elements in left subtree (mid),
         # and take only the elements before mid (root, taking the left subtree) from inorder
@@ -31,3 +33,35 @@ class Solution:
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
 
         return root
+
+    # O(n) solution w/ hashmap optimization
+    # Definition for a binary tree node.
+
+    def buildTreeHashMap(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # hashmap for fast lookup for inorder indexes - search is now O(1)
+        indices = {val: idx for idx, val in enumerate(inorder)}
+        
+        self.pre_idx = 0 # track preorder index
+        # recursive dfs
+        def dfs(l, r):
+            # base case: if l > r, then we've traversed through all elements
+            if l > r: 
+                return None
+
+            # root is the first element of preorder 
+            # increment preorder index for next iteration after picking root val
+            # creating the root as a treenode
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1 
+            root = TreeNode(root_val)
+            
+            # finding the mid point index in inorder
+            # all elements to the left of mid in inorder (l to mid - 1) are in the left subtree
+            # all elements to the right of mid in inorder (mid + 1 to r) are in the right subtree
+            mid = indices[root_val]
+            root.left = dfs(l, mid - 1)
+            root.right = dfs(mid + 1, r)
+            
+            return root
+
+        return dfs(0, len(inorder) - 1) 
